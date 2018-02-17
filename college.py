71a3bdb65aa055e72a9559ece14f11c11f93a032
henryz2000 <hzheng3@stuy.edu>
2018-02-16 19:58:51 -0500

    finished

--- college.py
+++ college.py
@@ -1,46 +1,64 @@
 import pymongo
-import urllib, json
+import json
+import requests
 
-url = "https://www.reddit.com/r/ApplyingToCollege.json"
-response = urllib.urlopen(url)
-#data = json.load(response)
-#rint response.read()
+
+link = 'https://www.reddit.com/r/ApplyingToCollege.json'
+r = requests.get(link, headers = {'User-agent': 'fourthTermJuniors'})
+data = r.json()
+posts = data["data"]["children"]
+#print json.dumps(posts, indent=4, sort_keys=True)
+
 
 connection = pymongo.MongoClient("149.89.150.100")
 db = connection.fourthTermJuniors
 collection = db.a2c
-
-with open("ApplyingToCollege.json", "r") as file:
-    parsed = json.load(file)
-    
-print json.dumps(parsed, indent=4, sort_keys=True)
+#print collection
 
 
-def boro(name):
-    cursor = collection.find({ "borough" : name, "borough." + name + ".name" : {"$ne" : ""} } )
+def addToCollection(collection):
+    for each in posts:
+        #print collection.update({ "data.title" : each["data"]["title"] }, each, upsert=True)
+        collection.update({ "data.title" : each["data"]["title"] }, each, upsert=True)
+
+        
+def suggested_sort(suggested):
+    print "==========printing posts with suggested_sort = " + suggested + "...=========="
+    cursor = collection.find({ "data.suggested_sort" : suggested })
     for i in cursor:
-        print i["name"]
-        #print i["borough"]
-
-def zipcode(code):
-    cursor = collection.find({ "address.zipcode" : code })
+        print "\t" + i["data"]["title"]
+        
+def upvotes(number):
+    print "==========printing posts with upvotes > " + str(number) + "...=========="
+    cursor = collection.find({ "data.ups" : {"$gt" : number} })
     for i in cursor:
-        print i["name"]
-        #print i["address"]["zipcode"]
+        print "\t" + str(i["data"]["ups"]) + ": " + i["data"]["title"]
 
-def zipgrade(code, grade):
-    cursor = collection.find({ "address.zipcode" : code, "grades.grade" : grade })
+def score(number):
+    print "==========printing posts with score > " + str(number) + "...=========="
+    cursor = collection.find({ "data.score" : {"$gt" : number} })
     for i in cursor:
-        print i["name"]
-        #print i["grades"][0]["grade"]
+        print "\t" + str(i["data"]["score"]) + ": " + i["data"]["title"]
+        
+def stickied():
+    print "==========printing stickied posts...=========="
+    cursor = collection.find({ "data.stickied" : True })
+    for i in cursor:
+        print "\t" + i["data"]["title"]
 
-def zipbelowscore(code, score):
-    cursor = collection.find({ "address.zipcode" : code, "grades.score" : {"$lt" : score} })
+def nonstickied():
+    print "==========printing nonstickied posts...=========="
+    cursor = collection.find({ "data.stickied" : False })
     for i in cursor:
-        print i["name"]
-        #print i["grades"][0]["score"]
-                
-#boro("Manhattan")
-#zipcode("10282")
-#zipgrade("10282", "A")
-#zipbelowscore("10282", 2)
+        print "\t" + i["data"]["title"]
+
+
+addToCollection(collection)
+
+suggested_sort("confidence")
+upvotes(15)
+upvotes(300)
+score(15)
+score(300)
+stickied()
+nonstickied()
